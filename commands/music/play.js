039352f0e77957e8a14b2bf1ca70b7ba47700314
TwelveMoons <44752373+TwelveZeros@users.noreply.github.com>
2021-02-01 01:22:41 +0700

    Add files via upload

--- commands/music/play.js
+++ commands/music/play.js
@@ -12,6 +12,6 @@ module.exports = {
         if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`);
         
         client.player.play(message, args.join(" "), { firstResult: true });
-        await message.delete()
+        const newLocal = await message.delete();
     },
 };
